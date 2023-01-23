b40cd1798bf1b6bc68b595e731479bcdc12d6bb8
gitJanaeW <janae.wel@gmail.com>
2023-01-23 15:57:05 -0500

    railway attempt

--- config/connections.js
+++ config/connections.js
@@ -1,8 +1,6 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    dialect: 'mysql',
-});
+const sequelize = new Sequelize(process.env.MYSQL_URL);
 
 module.exports = sequelize;
